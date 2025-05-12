1b24a00586a64832e870078bf28732ed786d51ad
andreaamasio <101635283+andreaamasio@users.noreply.github.com>
2025-05-12 06:51:01 +0200

    first API design with prisma schema

---
+++ API/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.get("/", (req, res) => res.send("Hello, world!"))
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`)
+})

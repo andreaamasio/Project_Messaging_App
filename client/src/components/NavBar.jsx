3772f152439571cdc8125e80b016a4889d906b30
andreaamasio <101635283+andreaamasio@users.noreply.github.com>
2025-05-18 05:25:37 +0200

    set up of react enviroment in client added simple navbar component

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import "./NavBar.css"
+
+function NavBar() {
+  return (
+    <>
+      <ul className="nav-list">
+        <li>Placeholder</li>
+        <li>
+          <h1>Messaging App</h1>
+        </li>
+        <li>
+          <a href="https://github.com/andreaamasio">
+            <img src="/pictures/github-60.svg" alt="Github-link" />
+          </a>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default NavBar
